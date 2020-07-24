c9be74005661f479bc70dcdd15117356586db13e
WorkPC <a.e.musaev@outlook.com>
2020-07-24 16:43:37 +0300

    Change distribution method

--- winApp/Resources/Calculate.cs
+++ winApp/Resources/Calculate.cs
@@ -14,25 +14,24 @@ namespace winApp.Resources
     {
         string subName;
         public string SubName { get => subName; set => subName = value; }
-        List<double> calculatedPrices;
-        List<double[]> priceLists;
+        List<int> calculatedPrices;
+        List<int[]> priceLists;
         int [] results;
         double summ;
-        double cost;
+        int cost;
         bool find = false;
         int mult = 1;
-
-
+        int[] distributedValues;
+        List<Product> prod;
         public Calculate()
         {
-            priceLists = new List<double[]>();
-            calculatedPrices = new List<double>();
+            priceLists = new List<int[]>();
+            calculatedPrices = new List<int>();
         }
         public void StartCalculating(List<Product> products, double cost1)
         {
             
-            this.cost = cost1;
-            
+            this.cost = (int)(cost1 * 100);
             if (products.Count == 1)
             {
                 Output outPut = new Output();
@@ -41,164 +40,196 @@ namespace winApp.Resources
             }
             else
             {
-                int a = RepCalc();
-                if (cost - (100000 * a) % 2 == 0)
-                    mult = 2;
-                while (!find)
-                {
-                    CalculatePrice(products, cost - (100000 * a));
-                    results = new int[priceLists.Count];
-                    int[] multiplyCounter = new int[priceLists.Count];
-                    BulkheadLists(0, 100000, multiplyCounter);
-                    if (!find)
-                        continue;
-                    for (int j = 0; j < results.Length; j++)
-                    {
-                        results[j] *= a;
-                    }
-                    find = false;
-                    while (!find)
-                    {
+                prod = products;
+                PercentageDistribution();
+                ValueFind();
 
-                        multiplyCounter = new int[priceLists.Count];
-                        BulkheadLists(0, cost - (100000 * a), multiplyCounter);
-                        if (!find)
-                            break;
-                    }
-                }
-                
-                Output outPut = new Output();
-                outPut.LoadCalculatedData(results, products, calculatedPrices, cost);
-                outPut.OutputExit();
+                //Output outPut = new Output();
+                //outPut.LoadCalculatedData(results, products, cost);
+                //outPut.OutputExit();
             }
 
         }
         
 
 
-        void BulkheadLists(int i, double cost, int[] multiplyCounter)
-        {
-            cost = Math.Round(cost, 2);
-           if (priceLists[i].Length > multiplyCounter[i])
-            {
-                for (int k = priceLists[i].Length; k > 0 ; k--)
-                {
-                    SummCalculating(multiplyCounter);
-                    if (summ == cost && !find)
-                    {
-                        for (int j = 0; j < results.Length; j++)
-                        {
-                            results[j] += multiplyCounter[j];
+        //void BulkheadLists(int i, double cost, int[] multiplyCounter)
+        //{
+        //    cost = Math.Round(cost, 2);
+        //   if (priceLists[i].Length > multiplyCounter[i])
+        //    {
+        //        for (int k = priceLists[i].Length; k > 0 ; k--)
+        //        {
+        //            SummCalculating(multiplyCounter);
+        //            if (summ == cost && !find)
+        //            {
+        //                for (int j = 0; j < results.Length; j++)
+        //                {
+        //                    results[j] += multiplyCounter[j];
                             
-                        }
-                        find = true;
-                        return;
-                    }
-                    else if(find)
+        //                }
+        //                find = true;
+        //                return;
+        //            }
+        //            else if(find)
+        //            {
+        //                return;
+        //            }
+        //            if (summ > cost)
+        //            {
+        //                break;
+        //            }
+        //            if (calculatedPrices[i] < cost)
+        //            {
+
+        //                    if (multiplyCounter[i] + mult < priceLists[i].Length)
+        //                    {
+        //                        multiplyCounter[i] += mult;
+        //                    }
+        //            }
+        //            if (i+1 < multiplyCounter.Length)
+        //            {
+        //                BulkheadLists(i + 1, cost, multiplyCounter);
+        //            }
+        //        }
+        //        multiplyCounter[i] = 0;
+        //    }
+        //    else
+        //    {
+        //        BulkheadLists(i + 1, cost, multiplyCounter);
+        //    }
+        //}
+
+
+
+
+        void PercentageDistribution()
+        {
+            
+                int percent = 10000;
+                int tempCost = cost;
+                CalculatePrice(prod);
+                distributedValues = new int[calculatedPrices.Count];
+                for (int i = 0; i < calculatedPrices.Count; i++)
+                {
+                    if (percent > 0 && tempCost > 0)
                     {
-                        return;
+                        if (i + 1 == calculatedPrices.Count)
+                        {
+                            distributedValues[i] = tempCost;
+                            break;
+                        }
+                        else
+                        {
+                            int temp = new Random().Next(0, percent);
+                            distributedValues[i] = (cost / 10000) * temp;
+                            percent -= temp;
+                            tempCost -= distributedValues[i];
+                        }
                     }
-                    if (summ > cost)
+                    else
                     {
                         break;
                     }
-                    if (calculatedPrices[i] < cost)
-                    {
+                }
+        }
+            
 
-                            if (multiplyCounter[i] + mult < priceLists[i].Length)
-                            {
-                                multiplyCounter[i] += mult;
-                            }
-                    }
-                    if (i+1 < multiplyCounter.Length)
+        private void ValueFind()
+        {
+            int k = 0;
+            results = new int[calculatedPrices.Count];
+            while (k < calculatedPrices.Count)
+            {
+
+                if (distributedValues[k] > 0)
+                {
+                    calculatedPrices[k] = CalculatePrice(prod, k, distributedValues[k]);
+                    if (calculatedPrices[k] != 0)
                     {
-                        BulkheadLists(i + 1, cost, multiplyCounter);
+                        k++;
+                    }
+                    else
+                    {
+                        PercentageDistribution();
+                        k = 0;
                     }
                 }
-                multiplyCounter[i] = 0;
+                else
+                    k++;
+            }
+        }
+
+        //private void SummCalculating(int [] multiplyCounter)
+        //{
+        //    // Обнуление суммы при повторном проходе
+        //    summ = 0;
+        //    // Сложение стоимостей всех элементов 
+        //    for (int i = 0; i < multiplyCounter.Length; i++)
+        //    {
+        //        if (priceLists[i].Length > multiplyCounter[i])
+        //        {
+        //            summ += Math.Round((priceLists[i][multiplyCounter[i]]), 2);
+        //        }
+        //    }
+        //}
+        private int CalculatePrice(List<Product> products, int i, int value)
+        {
+            List<int> arr = new List<int>();
+            for (int counter = Convert.ToInt32(products[i].minPrice * 100); counter < Convert.ToInt32(products[i].maxPrice * 100); counter++)
+            {
+                if (value % counter == 0)
+                {
+                    arr.Add(counter);
+                }
+            }
+            if(arr.Count > 0)
+            {
+                return  arr[new Random().Next(0, arr.Count)];
             }
             else
             {
-                BulkheadLists(i + 1, cost, multiplyCounter);
+                return 0;
             }
         }
-
-
-
-
-        int RepCalc()
-        {
-            double input = cost;
-            int counter = 0;
-            while (input > 100000)
-            {
-                counter++;
-                input -= 100000;
-            }
-            return counter;
-        }
-
-
-        private void SummCalculating(int [] multiplyCounter)
-        {
-            // Обнуление суммы при повторном проходе
-            summ = 0;
-            // Сложение стоимостей всех элементов 
-            for (int i = 0; i < multiplyCounter.Length; i++)
-            {
-                if (priceLists[i].Length > multiplyCounter[i])
-                {
-                    summ += Math.Round((priceLists[i][multiplyCounter[i]]), 2);
-                }
-            }
-        }
-
-        private void CalculatePrice(List<Product> products, double cost)
+        private void CalculatePrice(List<Product> products)
         {
 
             // Инициализация при определение количество элементов в подклассе
             // Расчет стоимости каждого элемента подкласса в пределах ценового диапазона
             Random rand = new Random();
             calculatedPrices.Clear();
-            //if (Convert.ToInt32(cost) == Convert.ToDouble(cost))
-            //{
-            //    for (int i = 0; i < products.Count; i++)
-            //    {
-            //        calculatedPrices.Add((double)rand.Next((Int32)products[i].minPrice, (Int32)products[i].maxPrice));
-            //    }
-            //}
-            //else
-            //{
+            
                 for (int i = 0; i < products.Count; i++)
                 {
-                    calculatedPrices.Add(Math.Round((rand.NextDouble() * (products[i].maxPrice - products[i].minPrice) + products[i].minPrice), 2));
-
+                
+                    calculatedPrices.Add(0);
                 }
-            //}
-            
+
+        
             // Обнуление счетчика повтора при перерасчете цен после неудачной итерации
             // Очистка списка расчитанных цен  после неудачной итерации
-            priceLists.Clear();
-            // Расчет таблицы цены - высчитывание множителя на кажду цену
-            for (int i = 0; i < products.Count; i++)
-            {
-                int k = 1;
-                List<double> priceMultiList = new List<double>();
-                priceMultiList.Add(0);
-                // цикл перемножений
-                while (calculatedPrices[i] * k < cost)
-                {
-                    // Возможно надо добавить проверку на превышение стоимости
-                    priceMultiList.Add((Math.Round(calculatedPrices[i] * k,2)));
-                    k += 1;
-                }
-                // Запись готового списка цен продукта в общий список
-                priceLists.Add(priceMultiList.ToArray());
+            //priceLists.Clear();
+            //// Расчет таблицы цены - высчитывание множителя на кажду цену
+            //for (int i = 0; i < products.Count; i++)
+            //{
+            //    int k = 1;
+            //    List<double> priceMultiList = new List<double>();
+            //    priceMultiList.Add(0);
+            //    // цикл перемножений
+            //    while (calculatedPrices[i] * k < cost)
+            //    {
+            //        // Возможно надо добавить проверку на превышение стоимости
+            //        priceMultiList.Add((Math.Round(calculatedPrices[i] * k,2)));
+            //        k += 1;
+            //    }
+            //    // Запись готового списка цен продукта в общий список
+            //    priceLists.Add(priceMultiList.ToArray());
+                
                 // создание ячеек счетчика согласно количеству продуктов в подкатегории
             }
 
 
-
         }
     }
-}
+
